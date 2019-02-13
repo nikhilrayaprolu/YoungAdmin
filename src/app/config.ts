a7b322e25c7aa55d61ed3d39e1d6281b0163c328
Nikhil Rayaprolu <nikhil684@gmail.com>
2019-02-13 12:40:25 +0530

    final changes

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,17 @@
-export let server = 'http://localhost:8000';
-export let LMS = 'http://localhost:18000'
-export let CMS = 'http://localhost:18010'
+let url = window.location.host;
+let url_split = url.match(/[^\.]*\.[^.]*$/);
+export let domain, server, LMS, CMS
+if (url_split) {
+  domain = url_split[0]
+  server = window.location.protocol + '//adminback.' + domain;
+  LMS = window.location.protocol + '//' + domain;
+  CMS = window.location.protocol + '//studio' + domain;
+
+} else {
+  server = 'http://localhost:8001';
+  LMS = 'http://localhost:18000';
+  CMS = 'http://studio.localhost:18010';
+}
+
+
+
